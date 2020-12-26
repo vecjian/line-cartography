5af99a9ba758c7d85169075714398e111abd7bd2
vecjian <1427681520@qq.com>
2020-12-26 18:14:59 +0800

    1226.1

---
+++ js/GL_Util1.js
@@ -0,0 +1,336 @@
+/**
+ * @author: prozac <516060659@qq.com>
+ * @createTime: 2018-4-2
+ * @copyRight: UNDEFINED
+ * @discription: A set of useful funcs for webGL-based development. Unlike some handy libs
+ * like three.js <https://threejs.org/> or phaser <http://www.phaserengine.com/>, webGL's
+ * original funcs are quite complicated and unfriendly for newbies.So here i select some
+ * commom funcs to form this simple lib.
+ * P.S. If possible, following methods need to be packaged into one GLOBAL variable
+ * like GL or sth else, i'm just tired of doing so XD
+ */
+
+/*------------------------------------------------------------------------------------*/
+
+/**
+ * create shader
+ * @method createShader
+ * @param  {context}    gl    core handle of webGL
+ * @param  {string}     type   [gl.VERTEX_SHADER || gl.FRAGMENT_SHADER]
+ * @param  {string}     source shader source code
+ * @return {shader}
+ */
+function createShader(gl, type, source) {
+  var shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+
+  gl.compileShader(shader);
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    throw new Error(gl.getShaderInfoLog(shader));
+  }
+
+  return shader;
+}
+
+/*------------------------------------------------------------------------------------*/
+
+/**
+ * create program
+ * @method createProgram
+ * @param  {context}     gl             core handle of webGL
+ * @param  {string}      vertexSource   code of vertex shader
+ * @param  {shader}      fragmentSource code of fragment shader
+ * @return {program}                    include a wrapper which is easy to set attribute of a program
+ */
+function createProgram(gl, vertexSource, fragmentSource) {
+  var program = gl.createProgram();
+
+  var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexSource);
+  var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentSource);
+
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    throw new Error(gl.getProgramInfoLog(program));
+  }
+
+  var wrapper = { program: program };
+
+  var numAttributes = gl.getProgramParameter(program, gl.ACTIVE_ATTRIBUTES);
+  for (var i = 0; i < numAttributes; i++) {
+    var attribute = gl.getActiveAttrib(program, i);
+    wrapper[attribute.name] = gl.getAttribLocation(program, attribute.name);
+  }
+  var numUniforms = gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS);
+  for (var i$1 = 0; i$1 < numUniforms; i$1++) {
+    var uniform = gl.getActiveUniform(program, i$1);
+    wrapper[uniform.name] = gl.getUniformLocation(program, uniform.name);
+  }
+
+  return wrapper;
+}
+
+/*------------------------------------------------------------------------------------*/
+
+/**
+ * create texture
+ * @method createTexture
+ * @param  {context}            gl     core handle of webGL
+ * @param  {string}             filter determine how to fill when zoom in or zoom out texture -> gl.LINERAR || gl.NEAREST
+ * @param  {image || array}     data   input data for texture
+ * @param  {int}                width  [optional] width of texture for array-type data rather than image-type
+ * @param  {int}                height [optional] height of texture for array-type data rather than image-type
+ * @return {texture}
+ */
+function createTexture(gl, filter, data, width, height) {
+  var texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, filter);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, filter);
+  if (data instanceof Uint8Array) {
+    gl.texImage2D(
+      gl.TEXTURE_2D,
+      0,
+      gl.RGBA,
+      width,
+      height,
+      0,
+      gl.RGBA,
+      gl.UNSIGNED_BYTE,
+      data
+    );
+  } else {
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, data);
+  }
+  gl.bindTexture(gl.TEXTURE_2D, null);
+  return texture;
+}
+
+/*------------------------------------------------------------------------------------*/
+
+/**
+ * bind texture to target index
+ * @method bindTexture
+ * @param  {context}    gl      core handle of webGL
+ * @param  {texture}    texture target texture
+ * @param  {int}        unit    target index of texture
+ */
+function bindTexture(gl, texture, unit) {
+  gl.activeTexture(gl.TEXTURE0 + unit);
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+}
+
+/*------------------------------------------------------------------------------------*/
+
+/**
+ * Bind Attribute
+ * @method bindAttribute
+ * @param  {context}     gl            core handle of webGL
+ * @param  {buffer}      buffer        framebuffer
+ * @param  {attribute}   attribute     target attribute
+ * @param  {int}         numComponents num of attributes to put in a period
+ */
+function bindAttribute(gl, buffer, attribute, numComponents) {
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.enableVertexAttribArray(attribute);
+  gl.vertexAttribPointer(attribute, numComponents, gl.FLOAT, false, 0, 0);
+}
+
+/*------------------------------------------------------------------------------------*/
+
+/**
+ * create buffer
+ * @method createBuffer
+ * @param  {context}     gl   core handle of webGL
+ * @param  {array}       data datasets which are to put in the buffer
+ * @return {buffer}
+ */
+function createBuffer(gl, data) {
+  var buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+  return buffer;
+}
+
+/*------------------------------------------------------------------------------------*/
+
+/**
+ * bind framebuffer to texture
+ * @method bindFramebuffer
+ * @param  {context}       gl           core handle of webGL
+ * @param  {buffer}        framebuffer  buffer plays a role of exchange-box
+ * @param  {texture}       texture      output of the buffer
+ */
+function bindFramebuffer(gl, framebuffer, texture) {
+  gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+  if (texture) {
+    gl.framebufferTexture2D(
+      gl.FRAMEBUFFER,
+      gl.COLOR_ATTACHMENT0,
+      gl.TEXTURE_2D,
+      texture,
+      0
+    );
+  }
+}
+
+/*------------------------------------------------------------------------------------*/
+
+/**
+ * fix Retina resolution bug(for its screen resolution is 2 while normal's is 1)
+ * @method fixRetina
+ * @param  {canvas}  canvas target canvas
+ */
+function fixRetina(canvas) {
+  var pxRatio = Math.max(Math.floor(window.devicePixelRatio) || 1, 2);
+  var ratio = pxRatio;
+  canvas.width = canvas.clientWidth * ratio;
+  canvas.height = canvas.clientHeight * ratio;
+}
+
+/*------------------------------------------------------------------------------------*/
+
+/**
+ * enable canvas to auto resize when window resize
+ * @method enableWindowResize
+ * @param  {[type]}           window [description]
+ * @return {[type]}                  [description]
+ */
+function enableWindowResize(window) {
+  // to be filled...
+}
+
+/*------------------------------------------------------------------------------------*/
+
+/**
+ * set GL's some common useful funcs
+ * @param {context} gl     core handle of webgl
+ */
+function setDefaultAssets(gl) {
+  gl.framebuffer = gl.createFramebuffer();
+  gl.quadBuffer = createBuffer(
+    gl,
+    new Float32Array([0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1])
+  );
+  gl.framebuffer.enable = function (outputTexture) {
+    if (typeof outputTexture == 'undefined') {
+      console.log('Please input a texture for the output of framebuffer!');
+      return;
+    }
+    bindFramebuffer(gl, gl.framebuffer, outputTexture);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  };
+  gl.framebuffer.disable = function () {
+    bindFramebuffer(gl, null);
+  };
+}
+
+/*------------------------------------------------------------------------------------*/
+
+/**
+ * create a quad texture(usually screen-type)
+ * @param {context} gl     core handle of webgl
+ * @param  {int} width      texture's width   [Default: window'width]
+ * @param  {int} height     texture's height  [Default: window's height]
+ * @return {texture}        the quad texture
+ */
+function createQuadTexture(gl, width, height) {
+  var w = width || gl.canvas.width;
+  var h = height || gl.canvas.height;
+
+  var num = w * h;
+  var emptyPixels = new Uint8Array(num * 4);
+  for (var i = 0; i < num * 4; i++) {
+    emptyPixels[i] = 255;
+  }
+
+  return createTexture(gl, gl.NEAREST, emptyPixels, w, h);
+}
+
+/*------------------------------------------------------------------------------------*/
+
+/**
+ * get context of webGL(gl)
+ * @method getGL
+ * @param  {window}     window global variable
+ * @param  {dict}       opt    assets of this webGL program
+ * eg.{
+ *      fixRetina{boolean}:              [default: true],
+ *      windowResize{boolean}:           [default: true],
+ *      clear{boolean}:                  [default: true],
+ *      preserveDrawingBuffer{boolean}:  [default: false],
+ * }
+ * @return {gl}                core handle of websGL
+ */
+function getGL(window, opt) {
+  var canvas = (window.canvas = document.getElementById('canvas'));
+  if (canvas == null) {
+    console.log(
+      "Warning: please set your webGL-output canvas's Id as 'canvas'!"
+    );
+    return;
+  }
+  var option = opt || {};
+  var fixretina = option.fixRetina || true;
+  var windowresize = option.windowResize || true;
+  var clear = option.clear || true;
+  var preserveDrawingBuffer = option.preserveDrawingBuffer || false;
+
+  var gl = (window.gl = canvas.getContext('webgl', {
+    // preserveDrawingBuffer: preserveDrawingBuffer,
+    antialias: true,
+  }));
+  gl.canvas = canvas;
+  if (!gl) {
+    console.log('Get GL Failed!');
+    return;
+  }
+  if (fixretina) {
+    fixRetina(canvas);
+  }
+  if (windowresize) {
+    enableWindowResize(window);
+  }
+  if (clear) {
+    clearGL(gl);
+  }
+
+  setDefaultAssets(gl, option);
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  return gl;
+}
+
+/*------------------------------------------------------------------------------------*/
+
+/**
+ * clear GL's buffer with specified color
+ * @method clearGL
+ * @param  {context} gl core handle of webGL
+ * @param  {float} r    color.r [default: 0.0]
+ * @param  {float} g    color.g [default: 0.0]
+ * @param  {float} b    color.b [default: 0.0]
+ * @param  {float} a    color.a [default: 0.0]
+ */
+function clearGL(gl) {
+  gl.clearColor(1, 1, 1, 1);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+/*------------------------------------------------------------------------------------*/
+
+/**
+ * clear a texture with specified color
+ * @param  {context} gl         core handle of webGL
+ * @param  {texture} texture    target texture
+ */
+function clearTexture(gl, texture) {
+  gl.framebuffer.enable(texture);
+  clearGL(gl);
+  gl.framebuffer.disable();
+}
+
+/*------------------------------------------------------------------------------------*/
---
+++ js/drawLine.js
@@ -0,0 +1,157 @@
+// 1.绘制线
+// 2.绘制三角网
+// 3.绘制变色后的三角形
+// 4.面状的色带缓冲区
+
+// 生成随机的线条
+function initLine(Pt_num) {
+  let pts_vector = [];
+  for (let i = 0; i < Pt_num; i++) {
+    let x = (Math.random() - 0.5) * 0.5;
+    let y = (Math.random() - 0.5) * 0.5;
+    let vec = new Point(x, y);
+    pts_vector.push(vec);
+  }
+  return pts_vector;
+}
+
+// 绘制，检测，重绘
+function draw(points, width) {
+  //计算了很多遍插值
+  let line = toXYArray(transform1(points));
+  let array_line = draw_line_Tris(points, width); //原始剖分三角形坐标
+  let array_overlaptri = draw_Triobjs(points, width); // 重叠三角形坐标
+  let array_debug = draw_debug_Trinet(points, width); //debug三角网坐标
+  /*
+    let line = toXYArray(points);
+    let array_line = draw_line_Tris(points, width); //原始剖分三角形坐标
+    let array_overlaptri = draw_Triobjs(points, width); // 重叠三角形坐标
+    let array_debug = draw_debug_Trinet(points, width); //debug三角网坐标
+*/
+  // 绘制
+  draw_three_objs(line, array_line, array_overlaptri, array_debug);
+}
+
+//绘制三角形串表示的线
+function draw_line_Tris(points, width) {
+  let pos = insertPts(points, width);
+  // let array = toXYArray(ptsToTriangles(pos.pts1, pos.pts2)); //得到所有组成线段的三角形坐标
+  let array = convertCor(toXYArray(ptsToTriangles(pos.pts1, pos.pts2))); //得到所有组成线段的三角形坐标
+  let arr = [];
+  arr.push(array);
+  return arr;
+}
+
+//  绘制debug三角网
+function draw_debug_Trinet(points, width) {
+  let pos = insertPts(points, width);
+  let array = ptsToTriangles(pos.pts1, pos.pts2); //得到所有组成线段的三角形坐标
+  let arr = [];
+  for (var i = 0; i < array.length; i = i + 3) {
+    let tri = [array[i], array[i + 1], array[i + 2]];
+    arr.push(tri);
+  }
+  let tri_xy = [];
+  for (var j = 0; j < arr.length; j++) {
+    tri_xy.push(toXYArray(arr[j])); //将三角形坐标转换成xy,数组
+  }
+
+  for (var i = 0; i < tri_xy.length; i++) {
+    tri_xy[i] = convertCor(tri_xy[i]);
+  }
+  // tri_xy = convertCor(tri_xy);
+  return tri_xy;
+  // 绘制三角网
+  // drawTriNet(tri_xy); //黑色边框
+}
+
+// 绘制重叠三角形
+function draw_Triobjs(points, width) {
+  let pos = insertPts(points, width);
+  let array = get_Tris(pos.pts1, pos.pts2); //得到所有组成线段的三角形坐标
+  let overlapTris = get_Overlap_Tris(array); //找出所有产生重叠的三角形
+  console.log(overlapTris);
+  array = Tris_to_XYarr(overlapTris); //将三角形坐标转换成xy数组
+
+  // 绘制变颜色的重叠三角形
+  // let color = [1.0, 0.0, 0.0, 1.0]; //重叠三角形变成红色，直接在绘制函数中改变颜色
+  let arr = [];
+  arr.push(array);
+
+  return arr;
+  // drawRiver(arr, color);
+}
+
+// 遍历每个三角形，比较得到重叠的三角形
+// para:   triangles={{p1,p2,p3}，{}，{}，……},为Triangle对象数组
+function get_Overlap_Tris(triangles) {
+  let len = triangles.length;
+
+  // 保存找到的有位置重叠的三角形
+  let overlap_Tris = [];
+  var flag = false;
+
+  // let arr = triangles; //将数组赋值给另外一个数组，可能会改变数组的值
+
+  for (var i = 0; i < len; i++) {
+    //triangles[j]不与前面的三角形发生重叠
+    for (var j = i + 1; j < len && !triangles[j].tag; j++) {
+      // 判断两个三角形的位置关系
+      flag = Trianglesoverlap(triangles[i], triangles[j]);
+
+      // flag=true,有位置重叠
+      if (flag) {
+        triangles[i].tag = true;
+        triangles[j].tag = true;
+        // 保存两个重叠的三角形
+        overlap_Tris.push(triangles[i]);
+        overlap_Tris.push(triangles[j]);
+      }
+    }
+  }
+  if (overlap_Tris.length < 1) {
+    console.log('折线没有重叠部分');
+    overlap_Tris.length = 0; //清空数组
+  }
+  return overlap_Tris;
+}
+
+//三角形对象数组转换为xy坐标，方便绘制
+function Tris_to_XYarr(triangles) {
+  let len = triangles.length;
+  if (triangles.length < 1) {
+    console.log('没有重叠三角形');
+    triangles.length = 0; //清空数组
+    return triangles;
+  }
+  let xyArr = [];
+
+  for (var i = 0; i < len; i++) {
+    let ABC = [];
+    ABC = Triangle.get_Tris_XYarr(triangles[i]);
+    xyArr = xyArr.concat(toXYArray(ABC)); //数组拼接
+  }
+
+  //转换坐标
+  xyArr = convertCor(xyArr);
+  console.log(xyArr);
+
+  return xyArr;
+}
+
+function convertCor(xyArr) {
+  let arr = [];
+  for (var i = 0; i < xyArr.length; i = i + 2) {
+    let x =
+      ((2 * (xyArr[i] - boundary.minX)) / (boundary.maxX - boundary.minX) - 1) *
+      0.95;
+    arr.push(x);
+    // console.log(xyArr[i + 1])
+    let y =
+      ((2 * (xyArr[i + 1] - boundary.minY)) / (boundary.maxY - boundary.minY) -
+        1) *
+      0.95;
+    arr.push(y);
+  }
+  return arr;
+}
